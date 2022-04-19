432e41a24e6b5f9410ee03566e979660f5f94bfa
tanisha111 <92454871+tanisha111@users.noreply.github.com>
2022-04-19 23:09:58 +0500

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+<div class="display">
+  <input v-model="operand1"/>
+  <input v-model="operand2"/>
+  = {{result}}
+</div>
+<div class="keyboard">
+ 
+  <button v-on:click ="result = operand1 - operand2">-</button>
+  <button v-on:click ="result = operand1 / operand2">/</button>
+  <button v-on:click ="result = operand1 * operand2">*</button>
+  <button v-on:click ="result = +operand1 + +operand2">+</button>
+  <button v-on:click ="result = operand1 ** operand2">**</button>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'super-calculator',
+  data () {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0,
+      
+      
+      
+    }
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
